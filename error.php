e4680ea06191b97667f4a228880c958ff14cf6b4
Lewis Jang <lewis.aedes@gmail.com>
2016-02-07 06:41:36 +0000

    update some file

--- error.php
+++ error.php
@@ -1,7 +1,7 @@
 #
 #<?php die('Forbidden.'); ?>
-#Date: 2016-02-07 01:44:32 UTC
+#Date: 2016-02-07 05:33:55 UTC
 #Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
 
 #Fields: datetime	priority clientip	category	message
-2016-02-07T01:44:32+00:00	INFO 178.211.176.111	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2016-02-07T05:33:55+00:00	INFO 178.211.176.111	joomlafailure	帳號與密碼不正確，或是您沒有帳號
